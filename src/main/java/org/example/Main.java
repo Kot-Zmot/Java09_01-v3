c1e22ef4a6c0b198ca2fb98ead36f16cb636b3bf
Kot-Zmot <meledin.av52@gmail.com>
2022-11-13 17:15:19 +0300

    is the code working?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import ru.netology.FormDate;
+import ru.netology.Post;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Post post = new Post();
+        post.name = "Иван";
+        // заполните другие поля
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 9;
+        post.birthday.year = 1980;
+
     }
 }
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    // добавьте другие поля
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+    // public String "birthday": {"day" : 13, "month": 6, "year": 1999};
+
+
+}
